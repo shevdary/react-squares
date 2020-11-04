9bf81dfedf385db5263998a0c5403053b73f4769
Daryna <shevvdary@gmail.com>
2020-11-04 21:08:00 +0200

    add mouseEvents

--- src/components/drawTable.jsx
+++ src/components/drawTable.jsx
@@ -1,69 +1,157 @@
-import React, { useState , Component} from "react";
+import React, { useState, Component } from "react";
 import "./style.scss";
-import { DrawButton } from "./drawButton";
 
 export default class DrawTable extends Component {
-    constructor(props) {
-        super(props);
-        const { initialWidth, initialHeight, cellSizse } = this.props;
-        this.state = {
-            tableData: {
-                col: [],
-                row: []
-            },
-            tableData2: {
-                col: [],
-                row: []
-            }
-        };
-        this.test(initialWidth, initialHeight, cellSizse);
-    }
-    test = (initialWidth, initialHeight, cellSizse) => {
-        for (let i = 0; i < initialHeight; i++)
-            this.state.tableData.row.push({ rowId: i });
-        for (let j = 0; j < initialWidth; j++)
-            this.state.tableData.col.push({ colId: j });
+  constructor(props) {
+    super(props);
+    const { initialWidth, initialHeight } = this.props;
+    this.state = {
+      tableData: {
+        col: [],
+        row: []
+      },
+
+      deleteColIndex: null,
+      deleteRowIndex: null,
+      isVisibleButtonRow: false,
+      isVisibleButtonCol: false,
+      isLastElementRow: false,
+      isLastElementCol: false
     };
-    renderTableData() {
-        return this.state.tableData.col.map(indexCol => {
+    this.test(initialWidth, initialHeight);
+  }
+  test = (initialWidth, initialHeight) => {
+    for (let i = 0; i < initialHeight; i++)
+      this.state.tableData.row.push({ i });
+    for (let j = 0; j < initialWidth; j++) this.state.tableData.col.push({ j });
+  };
+  renderTableData() {
+    return this.state.tableData.row.map(indexRow => {
+      return (
+        <tr
+          key={indexRow.id}
+          style={{ width: this.props.cellSize, height: this.props.cellSize }}
+        >
+          {this.state.tableData.col.map((indexCol, i) => {
             return (
-                <tr key={indexCol}>
-                    {this.state.tableData.row.map((indexRow, i) => {
-                        return <td key={indexRow}></td>;
-                    })}
-                </tr>
+              <td
+                key={indexCol.id}
+                style={{
+                  width: this.props.cellSize,
+                  height: this.props.cellSize
+                }}
+              />
             );
-        });
-    }
-    addedCol = e => {
-        e.preventDefault();
-        let copyTable = this.state.tableData;
-        copyTable.col.push({});
-        this.setState({ tableData: copyTable });
-    };
-    addedRow = e => {
-        e.preventDefault();
-        let copyTable = this.state.tableData;
-        copyTable.row.push({});
-        this.setState({ tableData: copyTable });
-    };
-render() {
+          })}
+        </tr>
+      );
+    });
+  }
+  addedCol = e => {
+    e.preventDefault();
+    let copyTable = this.state.tableData;
+    copyTable.col.push({});
+    this.setState({ tableData: copyTable });
+  };
+  addedRow = e => {
+    e.preventDefault();
+    let copyTable = this.state.tableData;
+    copyTable.row.push({});
+    this.setState({ tableData: copyTable });
+  };
+
+  deleteRow = indexRow => {
+    const findDeleteRow = this.state.tableData;
+    const rowStart = this.state.tableData.row.slice(0, indexRow);
+    const rowEnd = this.state.tableData.row.slice(indexRow + 1);
+    findDeleteRow.row = rowStart.concat(rowEnd);
+    console.log(this.state.tableData.row[1]);
+    this.setState({ tableData: findDeleteRow });
+    this.mouseLeave();
+  };
+
+  deleteColl = indexColl => {
+    const findDeleteColl = this.state.tableData;
+    const collStart = this.state.tableData.col.slice(0, indexColl);
+    const collEnd = this.state.tableData.col.slice(indexColl + 1);
+    findDeleteColl.col = collStart.concat(collEnd);
+    this.setState({ tableData: findDeleteColl });
+    this.displayButton();
+    this.mouseLeave();
+  };
+  mouseOverEvent = e => {
+    const buttonPositionY = document.querySelector(".remove-row");
+    const buttonPositionX = document.querySelector(".remove-coll");
+    const deleteRowIndex = e.target.parentElement.rowIndex;
+    const deleteColIndex = e.target.cellIndex;
+    buttonPositionY.style.top = e.target.offsetTop + "px";
+    buttonPositionX.style.left = e.target.offsetLeft + "px";
+    this.setState({ deleteRowIndex, deleteColIndex });
+    this.mouseEnter();
+    this.displayButton();
+  };
+  mouseLeave = () => {
+    this.setState({
+      isVisibleButtonRow: false,
+      isVisibleButtonCol: false
+    });
+  };
+  mouseEnter = () => {
+    this.setState({
+      isVisibleButtonRow: true,
+      isVisibleButtonCol: true
+    });
+  };
+  displayButton = () => {
+    this.setState({
+      isLastElementRow: this.state.tableData.row[1] ? true : false,
+      isLastElementCol: this.state.tableData.col[1] ? true : false
+    });
+  };
+
+  render() {
+    const {
+      deleteRowIndex,
+      deleteColIndex,
+      isVisibleButtonRow,
+      isVisibleButtonCol,
+      isLastElementRow,
+      isLastElementCol
+    } = this.state;
+    const hideRow = isVisibleButtonRow ? "visible" : "hidden";
+    const hideCol = isVisibleButtonCol ? "visible" : "hidden";
+    const displayBtnRow = isLastElementRow ? "block" : "none";
+    const displayBtnCol = isLastElementCol ? "block" : "none";
     return (
-        <div className="squares-table">
-            <table>
-                <tbody>{this.renderTableData()}</tbody>
-            </table>
-            <button className="button-add add-row" onClick={e => this.addedCol(e)}>
-                +
-            </button>
-            <button className="button-add add-coll" onClick={e => this.addedRow(e)}>
-                +
-            </button>
-            <button className="button-rm remove-row">-</button>
-            <button className="button-rm remove-coll">-</button>
-        </div>
+      <div className="squares-table" onMouseLeave={this.mouseLeave}>
+        <table onMouseEnter={this.mouseEnter}>
+          <tbody onMouseMove={this.mouseOverEvent}>
+            {this.renderTableData()}
+          </tbody>
+        </table>
+        <button className="button-add add-coll" onClick={e => this.addedCol(e)}>
+          +
+        </button>
+        <button className="button-add add-row" onClick={e => this.addedRow(e)}>
+          +
+        </button>
+        <button
+          className={`button-rm remove-coll ${hideCol} ${displayBtnCol}`}
+          onClick={e => {
+            this.deleteColl(deleteColIndex);
+          }}
+          onMouseEnter={this.mouseEnter}
+        >
+          -
+        </button>
+        <button
+          className={`button-rm remove-row ${hideRow}  ${displayBtnRow}`}
+          onClick={e => this.deleteRow(deleteRowIndex)}
+          onMouseEnter={this.mouseEnter}
+        >
+          -
+        </button>
+      </div>
     );
+  }
 }
-};
-
-
